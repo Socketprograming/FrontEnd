45bcec142d3a3688b74fdf0110bba2135a9e1771
parkyejin <uiop5809@naver.com>
2023-11-16 10:47:05 +0900

    feat: 주식 정보 설정 페이지 제작

---
+++ src/constants/stockInformation.ts
@@ -0,0 +1,42 @@
+export const stockInformations = [
+  {
+    id: 1,
+    stock: '삼성수산',
+  },
+  {
+    id: 2,
+    stock: '삼성디지털이미징',
+  },
+  {
+    id: 3,
+    stock: '삼성중공업',
+  },
+  {
+    id: 4,
+    stock: '삼성생명',
+  },
+  {
+    id: 5,
+    stock: '삼성화재해상보험',
+  },
+  {
+    id: 6,
+    stock: '삼성카드',
+  },
+  {
+    id: 7,
+    stock: '삼성바이오로직스',
+  },
+  {
+    id: 8,
+    stock: '삼성머스트스팩5호',
+  },
+  {
+    id: 9,
+    stock: '삼성스팩4호',
+  },
+  {
+    id: 10,
+    stock: '삼성전자',
+  },
+];
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,130 @@
+import styled from 'styled-components';
+import { stockInformations } from '../constants/stockInformation';
+
 const MainPage = () => {
+  const handleSearchButtonClick = () => {
+    console.log('검색 버튼 클릭');
+  };
+
   return (
-    <div>
-      <h1>MainPage</h1>
-    </div>
+    <MainPageContainer>
+      <MainTitle>Notify</MainTitle>
+      <MainSubTitle>주식 정보 설정</MainSubTitle>
+      <MainDescription>주식 정보는 매일 아침 9시와 오후 3시 30분에 발송됩니다.</MainDescription>
+      <MainSearchForm>
+        <MainSearchInput onClick={handleSearchButtonClick} />
+        <MainSearchButton onClick={handleSearchButtonClick}>검색</MainSearchButton>
+        <MainSearchButton>내 관심목록</MainSearchButton>
+      </MainSearchForm>
+
+      <MainContentTitle>
+        <div>#</div>
+        <div>종목 명</div>
+        <div>추가</div>
+      </MainContentTitle>
+      <MainContent>
+        {stockInformations.map((stockInformation) => (
+          <MainContentTitle key={stockInformation.id}>
+            <div>{stockInformation.id}</div>
+            <div>{stockInformation.stock}</div>
+            <PlusButton>관심종목 추가</PlusButton>
+          </MainContentTitle>
+        ))}
+      </MainContent>
+    </MainPageContainer>
   );
 };
 
 export default MainPage;
+
+const MainPageContainer = styled.div`
+  width: 40rem;
+  margin: 0 auto;
+  background: #eff6f5;
+  padding: 1rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const MainTitle = styled.div`
+  font-size: 2.5rem;
+  font-weight: bold;
+  padding: 0.5rem 0;
+`;
+
+const MainSubTitle = styled.div`
+  font-size: 1.3rem;
+`;
+
+const MainDescription = styled.div`
+  font-size: 1.2rem;
+  padding: 1.5rem 0;
+`;
+
+const MainSearchForm = styled.form`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-bottom: 1rem;
+`;
+
+const MainSearchInput = styled.input`
+  width: 20rem;
+  border: 1px solid lightgray;
+  border-radius: 0.5rem;
+  font-size: 1rem;
+  padding: 0.6rem 1rem;
+  box-sizing: border-box;
+`;
+
+const MainSearchButton = styled.button`
+  padding: 0.4rem 1rem;
+  border: 1px solid #00770c;
+  color: #00770c;
+  border-radius: 0.5rem;
+  font-size: 1rem;
+  white-space: nowrap;
+`;
+
+const MainContentTitle = styled.div`
+  width: 100%;
+  border-bottom: 1px solid lightgray;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 0;
+
+  div {
+    width: 100%;
+    text-align: center;
+  }
+`;
+
+const MainContent = styled.div`
+  width: 100%;
+  border-bottom: 1px solid lightgray;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  div {
+    width: 100%;
+    text-align: center;
+  }
+`;
+
+const PlusButton = styled.button`
+  padding: 0.4rem 1rem;
+  border: 1px solid blue;
+  color: blue;
+  border-radius: 0.5rem;
+  font-size: 1rem;
+  white-space: nowrap;
+  width: 16rem;
+  margin: 0 2.5rem;
+`;
